279f0a215ea20ecfde57a0e5380344c6a9ac8d28
Romain Rastel <rromain35@gmail.com>
2017-09-10 17:10:43 +0200

    add some doc

--- src/DoLess.UriTemplates/UriTemplate.cs
+++ src/DoLess.UriTemplates/UriTemplate.cs
@@ -11,44 +11,91 @@ namespace DoLess.UriTemplates
         private readonly Dictionary<string, object> variables;
         private readonly string template;
 
-        private UriTemplate(string template, bool isVariableNameCaseSensitive)
+        private UriTemplate(string template, bool isParameterNameCaseSensitive)
         {
             this.template = template;
-            this.variables = new Dictionary<string, object>(isVariableNameCaseSensitive ? null : StringComparer.OrdinalIgnoreCase);
+            this.variables = new Dictionary<string, object>(isParameterNameCaseSensitive ? null : StringComparer.OrdinalIgnoreCase);
         }
 
+        /// <summary>
+        /// Gets the provided template.
+        /// </summary>
         public string Template => this.template;
 
+        /// <summary>
+        /// Gets the parameter names already provided.
+        /// </summary>
         public IReadOnlyCollection<string> ParameterNames => this.variables.Keys;
 
-        public static UriTemplate For(string template, bool isVariableNameCaseSensitive = true)
+        /// <summary>
+        /// Creates a new <see cref="UriTemplate"/> with the specified template.
+        /// </summary>
+        /// <param name="template">The template.</param>
+        /// <param name="isParameterNameCaseSensitive">Indicates whether two parameters with the same name but different cases should be the same (default <code>true</code>).</param>
+        /// <returns></returns>
+        public static UriTemplate For(string template, bool isParameterNameCaseSensitive = true)
         {
-            return new UriTemplate(template, isVariableNameCaseSensitive);
+            return new UriTemplate(template, isParameterNameCaseSensitive);
         }
 
+        /// <summary>
+        /// Remove the specified parameter.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <returns></returns>
         public UriTemplate WithoutParameter(string name)
         {
             this.variables.Remove(name);
             return this;
         }
 
-        public UriTemplate WithParameter<T>(string name, T value)
+        /// <summary>
+        /// Sets the parameter's value.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="value">The value associated with.</param>
+        /// <returns></returns>
+        public UriTemplate WithParameter(string name, object value)
         {
             this.variables[name] = value;
             return this;
         }
 
+        /// <summary>
+        /// Sets the parameter's values (as an array).
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="values">The values associated with.</param>
+        /// <returns></returns>
+        public UriTemplate WithParameter(string name, params object[] values)
+        {
+            this.variables[name] = values;
+            return this;
+        }
+
+        /// <summary>
+        /// Expands the variables an returns the resulting <see cref="string"/>.
+        /// </summary>
+        /// <returns></returns>
         public string ExpandToString()
         {
             TemplateProcessor templateProcessor = new TemplateProcessor(this.template, this.variables);
             return templateProcessor.Expand();
         }
 
+        /// <summary>
+        /// Expands the variables an returns the resulting <see cref="Uri"/>.
+        /// </summary>
+        /// <returns></returns>
         public Uri ExpandToUri()
         {
             return new Uri(this.ExpandToString());
         }
 
+        /// <summary>
+        /// Returns the template.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return this.template;
