390d850c05fe402368ae4e438deb517912275b8a
Romain Rastel <romain.rastel@live.fr>
2017-09-10 17:10:43 +0200

    add some doc

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # DoLess.UriTemplates
-.Net Standard 2.0 implementation of the URI Template Spec https://tools.ietf.org/html/rfc6570
+
+.Net Standard 2.0 implementation of the URI Template Spec [RFC6570](http://tools.ietf.org/html/rfc6570):
+
+* Supports up to level 4 templates epxression
+* Tested against all test cases from [UriTemplate test suite](https://github.com/uri-templates/uritemplate-test).
+* Fluent API
+* .Net Standard 2.0
+
+## Install
+
+Install via [Nuget package](https://www.nuget.org/packages/DoLess.UriTemplates)
+
+## Examples
+
+Resolve a URI template:
+
+```csharp
+string uriString = UriTemplate.For("http://example.org/{resource}{?genre,count}")
+                              .WithParameter("resource", "books")
+                              .WithParameter("genre", "sci-fi")
+                              .WithParameter("count", 10)
+                              .ExpandToString();
+uriString.ShouldBeEquivalentTo("http://example.org/books?genre=sci-fi&count=10");
+```
+
+You can pass a string list:
+
+```csharp
+string uriString = UriTemplate.For("http://example.org/{resource}{?genre}")
+                              .WithParameter("resource", "books")
+                              .WithParameter("genre", "sci-fi", "horror", "fantasy")
+                              .ExpandToString();
+uriString.ShouldBeEquivalentTo("http://example.org/books?genre=sci-fi,horror,fantasy");
+```
+
+```csharp
+string uriString = UriTemplate.For("http://example.org/{resource}{?genre*}")
+                              .WithParameter("resource", "books")
+                              .WithParameter("genre", "sci-fi", "horror", "fantasy")
+                              .ExpandToString();
+uriString.ShouldBeEquivalentTo("http://example.org/books?genre=sci-fi&genre=horror&genre=fantasy");
+```
+
+Or a string dictionary:
+
+```csharp
+Dictionary<string, string> options = new Dictionary<string, string>
+{
+    ["genre"] = "sci-fi",
+    ["count"] = "10",
+    ["author"] = "George R. R. Martin",
+};
+
+string uriString = UriTemplate.For("http://example.org/{resource}{?options*}")
+                              .WithParameter("resource", "books")
+                              .WithParameter("options", options)
+                              .ExpandToString();
+uriString.ShouldBeEquivalentTo("http://example.org/books?genre=sci-fi&count=10&author=George%20R.%20R.%20Martin");
+```
+
+When a parameter is not set, it simply removes it:
+
+```csharp
+string uriString = UriTemplate.For("http://example.org/{resource}{?genre,count}")
+                              .WithParameter("resource", "books")
+                              .WithParameter("count", 10)
+                              .ExpandToString();
+uriString.ShouldBeEquivalentTo("http://example.org/books?count=10");
+```
+
+## Roadmap/Ideas
+
+* Support partial resolve
---
+++ src/DoLess.UriTemplates.Tests/ExampleTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DoLess.UriTemplates.Tests
+{
+    [TestFixture]
+    public class ExampleTests
+    {
+        [Test]
+        public void Example01()
+        {
+            string uriString = UriTemplate.For("http://example.org/{resource}{?genre,count}")
+                                          .WithParameter("resource", "books")
+                                          .WithParameter("genre", "sci-fi")
+                                          .WithParameter("count", 10)
+                                          .ExpandToString();
+            uriString.ShouldBeEquivalentTo("http://example.org/books?genre=sci-fi&count=10");
+        }
+
+        [Test]
+        public void Example02()
+        {
+            string uriString = UriTemplate.For("http://example.org/{resource}{?genre}")
+                                          .WithParameter("resource", "books")
+                                          .WithParameter("genre", "sci-fi", "horror", "fantasy")
+                                          .ExpandToString();
+            uriString.ShouldBeEquivalentTo("http://example.org/books?genre=sci-fi,horror,fantasy");
+        }
+
+        [Test]
+        public void Example03()
+        {
+            string uriString = UriTemplate.For("http://example.org/{resource}{?genre*}")
+                                          .WithParameter("resource", "books")
+                                          .WithParameter("genre", "sci-fi", "horror", "fantasy")
+                                          .ExpandToString();
+            uriString.ShouldBeEquivalentTo("http://example.org/books?genre=sci-fi&genre=horror&genre=fantasy");
+        }
+
+        [Test]
+        public void Example04()
+        {
+            Dictionary<string, string> options = new Dictionary<string, string>
+            {
+                ["genre"] = "sci-fi",
+                ["count"] = "10",
+                ["author"] = "George R. R. Martin",
+            };
+
+            string uriString = UriTemplate.For("http://example.org/{resource}{?options*}")
+                                          .WithParameter("resource", "books")
+                                          .WithParameter("options", options)
+                                          .ExpandToString();
+            uriString.ShouldBeEquivalentTo("http://example.org/books?genre=sci-fi&count=10&author=George%20R.%20R.%20Martin");
+        }
+
+        [Test]
+        public void Example05()
+        {
+            string uriString = UriTemplate.For("http://example.org/{resource}{?genre,count}")
+                                          .WithParameter("resource", "books")
+                                          .WithParameter("count", 10)
+                                          .ExpandToString();
+            uriString.ShouldBeEquivalentTo("http://example.org/books?count=10");
+        }
+
+    }
+}
